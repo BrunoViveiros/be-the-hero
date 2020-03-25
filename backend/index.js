9fc060907b417c03a312f078f4cc8ec38ce5240e
Ray Zyphs <bruferviveiros@gmail.com>
2020-03-25 09:52:47 -0300

    configurando o nodemon

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
   return response.json({
     evento: 'Semana OmniStack 11.0',
-    aluno: 'Bruno Viveiros',
-  })
+    aluno: 'Bruno Viveiros'
+  });
 });
 
 app.listen(3333);
